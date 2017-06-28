ae3ef83a5ffda434132fb8bcf6fc64ec9faa0001
liuyan9 <liuyan9@yonyou.com>
2017-06-28 13:36:28 +0800

    init

---
+++ src/main/java/liu/yan/acm/Acm001.java
@@ -0,0 +1,16 @@
+package liu.yan.acm;
+
+import java.util.Scanner;
+
+/**
+ * Created by liuyan9 on 2017/6/28.
+ */
+public class ACM001 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a+b);
+    }
+}

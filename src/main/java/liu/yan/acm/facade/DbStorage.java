a7e7b774ecfef3f9eec57b01d89032a1d9ed6316
liuyan9 <liuyan9@yonyou.com>
2017-10-30 15:22:37 +0800

    设计模式门面模式

---
+++ src/main/java/liu/yan/acm/facade/DbStorage.java
@@ -0,0 +1,31 @@
+package liu.yan.acm.facade;
+
+/**
+ * 数据库存储
+ *
+ * @author 刘研
+ * @create 2017-10-30 15:14
+ **/
+public class DbStorage implements IStorage{
+
+  @Override
+  public void save(Object object) {
+    System.out.println("存储到数据库");
+  }
+
+  @Override
+  public void update(Object object) {
+
+  }
+
+  @Override
+  public void delete(Object object) {
+    System.out.println("从数据库删除");
+  }
+
+  @Override
+  public Object query(int id) {
+    System.out.println("从数据库中查询");
+    return null;
+  }
+}

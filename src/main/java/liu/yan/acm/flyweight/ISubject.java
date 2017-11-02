4592fa4a4510a7a0b3979e6845dedf04e61023b3
liuyan9 <liuyan9@yonyou.com>
2017-11-02 15:15:34 +0800

    设计模式享元模式

---
+++ src/main/java/liu/yan/acm/flyweight/ISubject.java
@@ -0,0 +1,16 @@
+package liu.yan.acm.flyweight;
+
+/**
+ * 抽象类
+ *
+ * @author 刘研
+ * @create 2017-11-02 14:58
+ **/
+public interface ISubject {
+
+  void setA(String a);
+
+  void setB(String b);
+  void setX(String X);
+  void method();
+}
